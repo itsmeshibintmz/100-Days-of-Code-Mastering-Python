210cdfae80190a18340399038f66e1a631a5d2ad
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-09-22 22:48:07 +0530

    comments

--- day-1-printing-start/main.py
+++ day-1-printing-start/main.py
@@ -1,1 +1,12 @@
-print("hello World!")
+print("Hello World!")
+
+"""
+print() function
+print("something") 
+print("hello")
+
+Strings
+--------
+Set of characters
+"" shows beginning and end of characters.
+"""
