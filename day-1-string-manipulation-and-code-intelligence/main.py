9de03144c83b7f3b57dd488723d2a6f29a6f7c88
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-09-22 23:07:31 +0530

    String manipulation

---
+++ day-1-string-manipulation-and-code-intelligence/main.py
@@ -0,0 +1,9 @@
+# 3 print statements to print 3 sentences
+
+print("Hello world!")
+print("Hello world!")
+print("Hello world!")
+
+# '\n' to print multiple sentences in a single print function
+
+print("Hello world!\nHello world!\nHello world!")
