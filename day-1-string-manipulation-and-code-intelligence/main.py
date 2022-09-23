92f4375e5ac6565d4966939a1db4b97b698d9ecc
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-09-23 15:54:37 +0530

    Update main.py

--- day-1-string-manipulation-and-code-intelligence/main.py
+++ day-1-string-manipulation-and-code-intelligence/main.py
@@ -1,10 +1,10 @@
 # 3 print statements to print 3 sentences
 
-"""
+'''
 print("Hello world!")
 print("Hello world!")
 print("Hello world!")
-"""
+'''
 
 # '\n' to print multiple sentences in a single print function
 
@@ -13,21 +13,28 @@ print("Hello world!")
 # Combine two Strings using '+' sign
 # Add space manually, otherwise it will merge the words together and show as one word. eg:helloshibin
 
-"""
+'''
 print("hello" + " shibin")
     # "or"
 print("hello" + " " + "shibin")
     # "or"
 print("hello " + "shibin")
-"""
+'''
 
 # above three print statements function the same
 
 # spaces are very important in python
 # for example,
-"""
+'''
  print("hello") #this throws an error called IndentationError: unexpected indent
-"""
+'''
+
+# syntax error
+'''
+example
+print("hello)
+'''
+
 
 
 
