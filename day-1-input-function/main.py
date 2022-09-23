10f7d36a8bc969182885413b70ab289fb40236af
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-09-23 17:13:12 +0530

    Update main.py

--- day-1-input-function/main.py
+++ day-1-input-function/main.py
@@ -1,5 +1,10 @@
 # print("what is your name?")
 
 # input funtion
+# input(A prompt from the user)
 
-input("what is your name?")
+# input("what is your name?")
+
+print("Hello " + input("what is your name?"))
+
+# Ctrl+K+C to comment code in Vscode
