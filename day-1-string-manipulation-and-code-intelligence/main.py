7c7cca841fdd7cf464c538b4bf737ee6786146d8
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-09-23 15:42:51 +0530

    Combine strings

--- day-1-string-manipulation-and-code-intelligence/main.py
+++ day-1-string-manipulation-and-code-intelligence/main.py
@@ -1,9 +1,19 @@
 # 3 print statements to print 3 sentences
 
+"""
 print("Hello world!")
 print("Hello world!")
 print("Hello world!")
+"""
 
 # '\n' to print multiple sentences in a single print function
 
-print("Hello world!\nHello world!\nHello world!")
+# print("Hello world!\nHello world!\nHello world!")
+
+# Combine two Strings using '+' sign
+# Add space manually, otherwise it will merge the words together and show as one word. eg:helloshibin
+print("hello" + " shibin")
+    #or"
+print("hello" + " " + "shibin")
+
+
