0d1be64d8280ede7a4023454eaabe94592b42048
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-09-23 16:21:54 +0530

    solution

--- day-1-string-manipulation-and-code-intelligence/debug.py
+++ day-1-string-manipulation-and-code-intelligence/debug.py
@@ -7,9 +7,13 @@ print(("New lines can be created with a backslash and n.")
 
 # Answer
 
+# added quotes at beginning 
 print("Day 1 - String Manipulation")
+# changed outer double quote tp single quote
 print('String Concatenation is done with the "+" sign.')
+# removed extra indentation
 print('e.g. print("Hello " + "world")')
+# removed extra bracket
 print("New lines can be created with a backslash and n.")
 
 
