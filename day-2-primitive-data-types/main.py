deb770480b5173364cc0a5df21b004ef3554df43
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-09-24 08:36:50 +0530

    strings

--- day-2-primitive-data-types/main.py
+++ day-2-primitive-data-types/main.py
@@ -1,2 +1,18 @@
 # print(len(12345)) // TypeError
 
+# Data Types
+
+# String
+
+print("Hello"[4])
+# prints the 4th character
+
+print("123"[1])
+# numbers insidedouble quotes will be considered as a string
+
+print("123" + "345")
+# output will be 123345
+
+print("Hello" + "123")
+# output will be Hello13
+
