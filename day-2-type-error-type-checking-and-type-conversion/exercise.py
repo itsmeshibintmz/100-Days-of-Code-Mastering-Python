a85f57424b355de423194c66858b853eb7803a44
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-09-24 09:25:56 +0530

    Update exercise.py

--- day-2-type-error-type-checking-and-type-conversion/exercise.py
+++ day-2-type-error-type-checking-and-type-conversion/exercise.py
@@ -1,6 +1,6 @@
 # Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
 
-two_digit_number = input("Enter a 2 digit nnumer")
+two_digit_number = input("Enter a 2 digit number: ")
 
 first_char = int(two_digit_number[0])
 second_char = int(two_digit_number[1])
