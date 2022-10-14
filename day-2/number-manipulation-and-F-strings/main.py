5754a09c79c56036d17f320fc5a15489b81fa7b0
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-10-14 19:52:58 +0530

    number manipulation

---
+++ day-2/number-manipulation-and-F-strings/main.py
@@ -0,0 +1,15 @@
+# print(8 / 3)
+# print(int(8 / 3))
+
+# to round the number
+
+print(round(8 / 3))
+# = 3 (2.6666666666666665)
+
+# we can specify the no.of digits of precision you want to round to
+
+print(round(8 / 3, 2))
+# = 2.67
+
+print(type(8 // 3)) # = 2 without converting to int
+
