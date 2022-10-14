efc6399c51dd30c4f2be06959d509dbfbc433239
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-10-14 20:23:06 +0530

    alternate solution

--- day-2/number-manipulation-and-F-strings/exercise.py
+++ day-2/number-manipulation-and-F-strings/exercise.py
@@ -16,3 +16,17 @@ weeks_left = (90 * 52) - (age * 52)
 months_left = (90 * 12) - (age * 12)
 
 print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left.")
+
+# atlernative answer
+
+# age = input("what is your current age: ")
+
+# age_as_int = int(age)
+
+# years_remaining = 90 - age_as_int
+# days_remaining = years_remaining * 365
+# weeks_remaining = years_remaining * 52
+# months_remaining = years_remaining * 12
+
+# message = f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months"
+# print(message)
