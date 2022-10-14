23613414402c6f78ba906744d697a077ea56f52e
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-10-14 15:54:43 +0530

    folder change

--- day-1-variable-naming/main.py
+++
@@ -1,6 +0,0 @@
-user_name = input("Enter your username: ")
-# variable name has to be one unit, can be seperated by _
-# can't use numbers at the beginning of a variable name
-# don't use privilleaged words like input, print etc...
-# use sensible names for future understanding eg: color,username,password etc...
-
