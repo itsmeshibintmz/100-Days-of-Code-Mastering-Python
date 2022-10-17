a673e4953cc7a1f80fca31642d668da14a97d4f8
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-10-17 21:46:51 +0530

    Update main.py

--- day-3/control-flow-with-if-else-and-conditional-operators/main.py
+++ day-3/control-flow-with-if-else-and-conditional-operators/main.py
@@ -16,9 +16,9 @@ else:
 """
 
 print("Welcome to the rollercoaster!")
-height = int(input("Enter your height in cm "))
+height = int(input("What is your height in cm? "))
 
-if height > 120:
+if height == 120:
     print("You can ride the rollercoaster!")
 else:
     print("Sorry, you can't ride the rollercoaster!")
@@ -34,3 +34,4 @@ else:
 ==  equal to
 !=  not equal to
 """
+
