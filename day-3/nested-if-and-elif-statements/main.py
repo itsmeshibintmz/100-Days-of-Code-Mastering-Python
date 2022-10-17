890a6cc49ea4f5ae45b3a1cccd8f85b9c9e2bbf3
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-10-17 22:05:19 +0530

    exercise

--- day-3/nested-if-and-elif-statements/main.py
+++ day-3/nested-if-and-elif-statements/main.py
@@ -9,3 +9,18 @@ if condition:
 else:
     do this
 """
+
+print("Welcome to the rollercoaster")
+
+height = int(input("What is your height in cm? "))
+age = int(input("What is your age? "))
+
+if height >= 120:
+    if age > 18:
+        print("You can ride the rollecaster!")
+        print("Please pay $12.")
+    else:
+        print("You can ride the rollercoaster!")
+        print("Please pay $7.")
+else:
+    print("You can't ride the rollercoaster!")
