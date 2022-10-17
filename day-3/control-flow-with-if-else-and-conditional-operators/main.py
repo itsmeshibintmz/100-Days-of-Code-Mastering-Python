c90bd5c835514c121d5c138d40ec7133b768036a
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-10-17 21:37:35 +0530

    example

--- day-3/control-flow-with-if-else-and-conditional-operators/main.py
+++ day-3/control-flow-with-if-else-and-conditional-operators/main.py
@@ -1,7 +1,17 @@
 # if else statement
 
-# if condition:
-    # do this
-# else:
-    # do this
+"""
+if condition:
+    do this
+else:
+    do this
+"""
+
+"""
+water_level = 50
+if water_level > 80:
+    print("Drain water")
+else:
+    print("Continue")
+"""
 
