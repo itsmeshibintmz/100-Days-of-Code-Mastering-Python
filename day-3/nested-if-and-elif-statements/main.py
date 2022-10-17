59270806a2c3b489b77cb4c9ebbd78a8bf0d7f21
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-10-17 22:14:19 +0530

    elif exercise

--- day-3/nested-if-and-elif-statements/main.py
+++ day-3/nested-if-and-elif-statements/main.py
@@ -10,19 +10,19 @@ else:
     do this
 """
 
-print("Welcome to the rollercoaster")
+# print("Welcome to the rollercoaster")
 
-height = int(input("What is your height in cm? "))
-age = int(input("What is your age? "))
+# height = int(input("What is your height in cm? "))
+# age = int(input("What is your age? "))
 
-if height >= 120:
-    print("You can ride the rollecaster!")
-    if age <= 18:
-        print("Please pay $7.")
-    else:
-        print("Please pay $12.")
-else:
-    print("You can't ride the rollercoaster!")
+# if height >= 120:
+#     print("You can ride the rollecaster!")
+#     if age <= 18:
+#         print("Please pay $7.")
+#     else:
+#         print("Please pay $12.")
+# else:
+#     print("You can't ride the rollercoaster!")
 
 # eif
 
@@ -36,3 +36,18 @@ else:
     do this
 """
 
+print("Welcome to the rollecaster!")
+
+height = int("What is your height in cm? ")
+age = int("What is your age? ")
+
+if height >= 120:
+    print("You can ride the rollecaster!")
+    if age < 12:
+        print("Please pay $5.")
+    elif age >=12 and age <=18:
+        print("Please pay $7..")
+    else:
+        print("Please pay $12.")
+else:
+    print("Sorry, you can't ride the rollecaster!")
