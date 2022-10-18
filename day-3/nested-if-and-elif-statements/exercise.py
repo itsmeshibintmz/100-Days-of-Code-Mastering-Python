930bfe981ff47b34cb9e83c2be9e313c96b56a37
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-10-18 15:54:41 +0530

    rounding up

--- day-3/nested-if-and-elif-statements/exercise.py
+++ day-3/nested-if-and-elif-statements/exercise.py
@@ -1,13 +1,13 @@
 height = float(input("Enter your height in m: "))
 weight = float(input("Enter your weight in kg: "))
 
-bmi = int(weight / (height ** 2))
+bmi = round((weight / (height ** 2)))
 
 # print(type(bmi))
 if bmi < 18.5:
-    print(f"Your bmi is {bmi}, you are underweight.")
+    print(f"Your BMI is {bmi}, you are underweight.")
 elif bmi < 25:
-        print(f"Your bmi is {bmi}, you have a normal weight.")
+        print(f"Your BMI is {bmi}, you have a normal weight.")
 elif bmi < 30:
         print(f"Your BMI is {bmi}, you are slightly overweight.")
 elif bmi < 35:
