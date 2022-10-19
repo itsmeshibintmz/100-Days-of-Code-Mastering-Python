d2c6611f03312496b8ace7b123f59e2b440c202e
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-10-19 20:24:32 +0530

    solution 2

--- day-4/pizza-order-practice/main.py
+++ day-4/pizza-order-practice/main.py
@@ -30,3 +30,24 @@ else:
     bill += 0
 
 print(f"Your total bill for the pizza is: ${bill}.")
+
+
+# or
+
+# bill = 0
+
+# if size == "S":
+#     bill += 15
+# elif size == "M":
+#     bill += 20
+# elif size == "L":
+#     bill += 25
+
+# if add_pepperoni == "Y":
+#     if size == "S":
+#         bill += 2
+#     else:
+#         bill += 3
+
+# if extra_cheese == "Y":
+#     bill += 1
