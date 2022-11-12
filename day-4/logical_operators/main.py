917500fd324b4957b3fc9f91cbf7fc6aaada3f4c
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-11-12 08:09:48 +0530

    logical operators

---
+++ day-4/logical_operators/main.py
@@ -0,0 +1,22 @@
+"""
+    if condition1 & condition2 & condition3
+        do this
+    else:
+        do this
+"""
+
+# Logical Operators
+"""
+    A and B
+    C or D
+    not E
+"""
+
+# if a > 10 & a < 13
+    # return true if both conditions are true, false if any one of the condition is false
+
+# or , if any of the condition is true, it is true
+
+# not, reverse the output 
+
+
