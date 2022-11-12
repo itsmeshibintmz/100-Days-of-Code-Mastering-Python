7417f1acb42ff8be2319c34b13cdacb1ee72e0d0
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-11-12 08:19:13 +0530

    try 2
    somewhat ok.

--- day-4/logical_operators/rollercoaster_exercise/main.py
+++ day-4/logical_operators/rollercoaster_exercise/main.py
@@ -1,17 +1,17 @@
 print("Welcome to rollercoaster!")
 
-height = int(input("What is your height in cm?"))
-age = int(input("What is your age?"))
+height = int(input("What is your height in cm ?"))
+age = int(input("What is your age ?"))
 bill = 0
 
 if height >= 120:
-    if age >= 18:
-        bill += 12
+    print("You can ride the rollecoaster!")
+    if age < 12:
+        bill += 5
     elif age >= 12 & age < 18:
         bill += 7
     else:
-        bill += 5
-    print("You can ride the rollecoaster!")
-    print(f"Your bill is ,{bill}")
+        bill += 12
+    print(f"Your bill is {bill}")
 else:
     print("Sorry, you can't ride the rollercoaster!")
