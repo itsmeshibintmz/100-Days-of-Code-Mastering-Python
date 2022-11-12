c9d9fecda1b6117db1aed94c8643ebacb29d0614
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-11-12 08:46:12 +0530

    solution
    🧠 this works btw.

--- day-4/logical_operators/rollercoaster_exercise/main.py
+++ day-4/logical_operators/rollercoaster_exercise/main.py
@@ -9,21 +9,21 @@ if height >= 120:
 
     age = int(input("What is your age? "))
     want_photo = input("Do you want a photo as a memory? Y or N? ")
-
-    if want_photo == "Y":
-        bill += 3
         
-    if age >= 45 and age <= 55:
-        bill = 0
-        print(f"Your bill is ${bill}")
-        print("Enjoy your ride.")
-    elif age < 12:
+    if age < 12:
         bill += 5
     elif age >= 12 & age < 18:
         bill += 7
     else:
         bill += 12
-
+    
+    if want_photo == "Y":
+        bill += 3
+    
+    if age >= 45 and age <= 55:
+        bill = 0
+        print(f"Your bill is ${bill}")
+        print("Enjoy your ride.")
     print(f"Your total bill is ${bill}")
     print("Enjoy your ride.")
 
