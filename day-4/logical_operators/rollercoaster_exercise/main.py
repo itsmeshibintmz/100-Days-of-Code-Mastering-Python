b7396650003264e88332bd210f8560a9376ab549
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-11-12 08:35:26 +0530

    bug fixes
    had some flaws in previous code:
    goes through whole data entering process regardless of the code.
    fixed by entering height only at first, and triggers other entities only after checking height.

--- day-4/logical_operators/rollercoaster_exercise/main.py
+++ day-4/logical_operators/rollercoaster_exercise/main.py
@@ -1,23 +1,25 @@
 print("Welcome to rollercoaster!")
 
 height = int(input("What is your height in cm? "))
-age = int(input("What is your age? "))
-want_photo = input("Do you want a photo as a memory? Y or N? ")
 
 bill = 0
 
 if height >= 120:
     print("You can ride the rollecoaster!")
+    age = int(input("What is your age? "))
+    want_photo = input("Do you want a photo as a memory? Y or N? ")
     if age < 12:
         bill += 5
     elif age >= 12 & age < 18:
         bill += 7
     else:
         bill += 12
+
+    if want_photo == "Y":
+        bill += 3
+
+    print(f"Your total bill is {bill}")
+
 else:
     print("Sorry, you can't ride the rollercoaster!")
 
-if want_photo == "Y":
-    bill += 3
-
-print(f"Your total bill is {bill}")
