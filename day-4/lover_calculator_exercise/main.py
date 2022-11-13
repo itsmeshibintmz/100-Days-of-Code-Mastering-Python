3ea44a777083708a1d9ac1b98ba901212f4899b0
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-11-13 21:40:45 +0530

    🧠🧠
    it's at least moving forward 📈

--- day-4/lover_calculator_exercise/main.py
+++ day-4/lover_calculator_exercise/main.py
@@ -3,5 +3,14 @@ print("Welcome to Love Calculator!")
 first_name = input("Enter your name: ")
 second_name = input("Enter your crush's name: ")
 
+count = 0
+
 for char in first_name:
-    print(char)
+    if char == "T":
+        count += 1
+
+for char in second_name:
+    if char == "T":
+        count += 1
+
+print(count)
