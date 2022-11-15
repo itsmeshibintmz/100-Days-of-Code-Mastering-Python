6182a817f27ba21a25c06f4764f6c1110c9bd583
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-11-15 21:45:07 +0530

    light upgrade

--- day-4/lover_calculator_exercise/main.py
+++ day-4/lover_calculator_exercise/main.py
@@ -6,11 +6,11 @@ second_name = input("Enter your crush's name: ")
 count = 0
 
 for char in first_name:
-    if char == "T":
+    if char == "T" or char == "R" or char == "U" or char == "E" or char == "t" or char == "r" or char == "u" or char == "e":
         count += 1
 
 for char in second_name:
-    if char == "T":
+    if char == "T" or char == "R" or char == "U" or char == "E" or char == "t" or char == "r" or char == "u" or char == "e":
         count += 1
 
 print(count)
