eb7b625b7395c398f40ddd0ca341affe57e316ce
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-11-16 11:52:26 +0530

    in progress

--- day-4/lover_calculator_exercise/main.py
+++ day-4/lover_calculator_exercise/main.py
@@ -3,14 +3,23 @@ print("Welcome to the Love Calculator!")
 name1 = input("What is your name?: ")
 name2 = input("What is  their name?: ")
 
-count = 0
+countTrue = 0
+countLove = 0
 
 for char in name1:
     if char == "T" or char == "R" or char == "U" or char == "E" or char == "t" or char == "r" or char == "u" or char == "e":
-        count += 1
+        countTrue += 1
 
 for char in name2:
     if char == "T" or char == "R" or char == "U" or char == "E" or char == "t" or char == "r" or char == "u" or char == "e":
-        count += 1
+        countTrue += 1
 
-print(count)
+for char in name1:
+    if char == "L" or char == "O" or char == "V" or char == "E" or char == "l" or char == "o" or char == "v" or char == "e":
+        countLove +=1
+
+## print(countTrue)
+
+if count < 40 :
+    print(f"Your score is {count}.")
+elif count
