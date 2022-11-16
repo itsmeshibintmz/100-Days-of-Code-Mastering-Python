d8fc0d891f70d2181b2ab27debc2b28d658c44f3
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-11-16 11:47:57 +0530

    question
    changing code according to question sample.

--- day-4/lover_calculator_exercise/main.py
+++ day-4/lover_calculator_exercise/main.py
@@ -1,15 +1,15 @@
-print("Welcome to Love Calculator!")
+print("Welcome to the Love Calculator!")
 
-first_name = input("Enter your name: ")
-second_name = input("Enter your crush's name: ")
+name1 = input("What is your name?: ")
+name2 = input("What is  their name?: ")
 
 count = 0
 
-for char in first_name:
+for char in name1:
     if char == "T" or char == "R" or char == "U" or char == "E" or char == "t" or char == "r" or char == "u" or char == "e":
         count += 1
 
-for char in second_name:
+for char in name2:
     if char == "T" or char == "R" or char == "U" or char == "E" or char == "t" or char == "r" or char == "u" or char == "e":
         count += 1
 
